7f06aaedfc57680dde0d6ffe9c5299da91ddf380
Lucas Daniel Pedroso <lukas_boo@hotmail.com>
2015-11-23 12:44:04 -0200

    Audio baixado funcionando

---
+++ app/src/main/java/conectguitar/music/tcc/com/conectguitar/Controller/Feedback.java
@@ -0,0 +1,7 @@
+package conectguitar.music.tcc.com.conectguitar.Controller;
+
+/**
+ * Created by lucas on 21/11/15.
+ */
+public class Feedback {
+}
---
+++ app/src/main/java/conectguitar/music/tcc/com/conectguitar/View/StudentsFeedback.java
@@ -0,0 +1,7 @@
+package conectguitar.music.tcc.com.conectguitar.View;
+
+/**
+ * Created by lucas on 20/11/15.
+ */
+public class StudentsFeedback {
+}
